71b2bd1fe50e7a6b1d5b61731775d75a35558233
iamayushy <ayushhuns@gmai.com>
2022-05-22 20:31:55 +0530

    throttle

--- js/debouncing.js
+++ js/debouncing.js
@@ -2,19 +2,32 @@ let search = document.querySelector('.search')
 let normal = document.querySelector('.normal')
 let debounce = document.querySelector('.debounce')
 let throttle = document.querySelector('.throttle')
-const goodfunction = canDoBetter(getData, 1000)
+const goodfunction = logger(getData, 1000)
 search.addEventListener('input', goodfunction)
 
 function getData(){
-    debounce.textContent = search.value
+    throttle.textContent = search.value
 }
-function canDoBetter(fun, delay){
-    let timeout
-    return (...args) => {
-        clearTimeout(timeout)
-        timeout = setTimeout(() => {
-            fun(...args)
-        }, delay)
+// function canDoBetter(fun, delay){
+//     let timeout
+//     return (...args) => {
+//         clearTimeout(timeout)
+//         timeout = setTimeout(() => {
+//             fun(...args)
+//         }, delay)
+//     }
+// }
+
+function logger(fun, delay){
+    let flag = true
+    return function(){
+        if(flag) {
+            fun()
+            flag = false
+            setTimeout(() => {
+                flag = true
+            }, delay)
+        }
+       
     }
 }
-
