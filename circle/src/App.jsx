868c2f89cf12de0f8a8e651a14939953b1091550
iamayushy <ayushhuns@gmai.com>
2022-05-29 20:57:36 +0530

    sorted colors

---
+++ circle/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import './App.css'
+import { Container } from './Components/Container'
+function App() {
+
+  return (
+    <div style={{textAlign:'center'}} className="App">
+      <h1>SORTED COLORS</h1>
+      <Container/>
+    </div>
+  )
+}
+
+export default App
--- keep/src/App.jsx
+++ keep/src/App.jsx
@@ -1,11 +1,49 @@
+import { Button, Input } from '@mantine/core'
+import { useState } from 'react'
 import './App.css'
-import { Note } from './Components/Note'
+import { TodoBox } from './Components/TodoBox'
 
 function App() {
+  const [click, setClick] = useState(true)
+  const [taskList, setTaskList] = useState([<TodoBox Title={'Ongoing'}/>])
+  const [handle, setHandle] = useState('')
+  const handleInput = (e) => {
+    if(handle.length < 4){
+      if(e.key === 'Enter'){
+        setClick(true)
+        return
+      }
+      
+    }
+    if(e.key === 'Enter'){
+      console.log(e.key)
+    setTaskList([
+      ...taskList,
+      <TodoBox Title={handle}/>
+    ])
+    setClick(true)
+  }
+  } 
 
   return (
     <div className="App">
-      <Note/>
+      {click ?
+     <Button onClick={() => setClick(false)}>Create New Board</Button> :
+     <Input onKeyPress={handleInput}  value={handle} onChange={(e)=>setHandle(e.target.value)} placeholder='Title'></Input>
+    }
+    <div className='tasklist'>
+     {taskList.map(ele => {
+       return(
+         <div>
+           {ele}
+           </div>
+       )
+     })}
+    </div>
+    <div>
+
+    </div>
+    
     </div>
   )
 }
